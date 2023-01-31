8d2f09207403d77c333a9d8779b783a0ed7998d2
Jesus Rafael Uribe Gallardo <jesus.uribe@oreillyauto.com>
2023-01-31 15:32:55 -0600

    first reactive basic form implemented

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre      : new FormControl('RTX 4080ti'),
+  //   precio      : new FormControl(1500),
+  //   existencias : new FormControl(5)
+  // })
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)] ],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  campoEsValido(control: string): boolean | null {
+    return this.miFormulario.controls[control].errors && this.miFormulario.controls[control].touched
+  }
+
+  guardar(){
+
+    if(this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset();
+  }
 }
