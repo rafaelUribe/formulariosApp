9de86c4d1cad1844b814fa9299ff1e0f825e38c1
Rafael Uribe <72708040+rafaelUribe@users.noreply.github.com>
2023-01-31 18:06:35 -0600

    to date

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class DinamicosComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]]
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  guardar(){
+
+  }
+
+  campoEsValido(control: string){
+    return this.miFormulario.controls[control].errors 
+    && 
+    this.miFormulario.controls[control].touched
+  }
 
 }
